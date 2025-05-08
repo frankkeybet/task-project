39c694266c8e19d5c0e348422883776e142e1e87
frankkeybet <94977450+frankkeybet@users.noreply.github.com>
2025-05-08 10:16:36 +0300

    Mutators and accessors the old and new way

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Task extends Model
 {
@@ -11,5 +13,39 @@ class Task extends Model
 
 
     protected $fillable = ['name'];
+
+    //old way for Accessors
+    // public function getNameAttribute($value)
+    // {
+    //    return Str::lower($value);
+    // }
+
+     /**Old way for Mutators */
+    // public function setNameAttribute($value)
+    // {
+    //     $this->attributes['name'] = Str::lower($value);
+    // }
+
+    /**New way for Accessors*/
+    // protected function name(): Attribute
+    // {
+    //     return Attribute::make(
+    //         // get: fn ($value) => Str::lower($value),
+    //         set: fn ($value) => Str::upper($value),
+    //     );
+    // }
+
+    /**New way for Mutators */
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            // get: fn ($value) => Str::kebab($value),
+            get: fn ($value) => Str::lower($value),
+            set: fn ($value) => Str::upper($value),
+        );
+    }
+
+   
 
 }
